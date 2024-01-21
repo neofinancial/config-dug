21fe54702d2c37486d5976ad0812a15ca8a554f6
Vinicius Maia <vinicius.rocha@neofinancial.com>
2024-01-20 20:51:09 -0700

    Undoing unnecessary changes

--- examples/script-esm-ts/index.ts
+++ examples/script-esm-ts/index.ts
@@ -4,12 +4,6 @@ const schema = {
   logLevel: z.string().default('info'),
 };
 
-const configDug = new ConfigDug(schema, { reloadInterval: 20000 });
+const config = await ConfigDug.getConfig(schema);
 
-await configDug.load();
-
-console.log('logLevel', configDug.getConfig().logLevel);
-
-configDug.on('config-reloaded', (config) => {
-  console.log('\n\nconfig-reloaded event received', config);
-});
+console.log('logLevel', config.logLevel);
