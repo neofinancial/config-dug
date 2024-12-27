d80eecb7dcd776bb6fc8279bf85a0650b3bfdb64
Aries Dimaranan <aries.dimaranan@neofinancial.com>
2024-12-27 16:13:53 -0700

    setup tests in root, updated CI

--- packages/config-dug/test/config-dug.test.ts
+++ packages/config-dug/test/config-dug.test.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+
 import { ConfigDug } from '../src/config-dug';
 
 describe('ConfigDug', () => {
--- packages/config-dug/test/vitest.config.ts
+++ packages/config-dug/test/vitest.config.ts
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineProject } from 'vitest/config';
 
-export default defineConfig({
+export default defineProject({
   test: {
     globals: true,
     environment: 'node',
     logHeapUsage: true,
     include: ['test/**/*.test.ts'],
     maxConcurrency: 16,
-    coverage: {
-      provider: 'v8',
-      include: ['src/*'],
-      exclude: [],
-      reporter: ['lcov', 'text', 'json-summary', 'html'],
-      reportsDirectory: './coverage',
-    },
   },
 });
